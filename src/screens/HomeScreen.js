c232c5188bc3550ebc195d4b07586cb19b9ff2ea
jonathanbrierre <jonathanbrierre@yahoo.com>
2020-06-18 15:07:52 -0400

    created first button & Touchable Opacity

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,28 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeSn</Text>;
+  return( 
+    <View>
+      <Text style={styles.text}>Hey Sexy</Text>
+      <Button 
+        onPress = {()=>console.log('You touched the booty')}
+        title='Booty'
+      />
+      <TouchableOpacity onPress = {() => console.log('Adele is the GOAT')}>
+        <Text style = {styles.buttonStyle}>Hello from the other side</Text>
+      </TouchableOpacity>
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30
+  },
+  buttonStyle: {
+    backgroundColor: 'red',
+    width: 200
   }
 });
 
